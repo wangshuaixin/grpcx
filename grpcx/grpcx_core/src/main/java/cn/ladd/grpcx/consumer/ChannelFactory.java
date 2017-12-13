a856d50ef3ae8d7fced925524b9a0519172da73c
ladd <ladd.cn@gmail.com>
2017-12-13 22:36:41 +0800

    Modifiy channel factory

--- grpcx/grpcx_core/src/main/java/cn/ladd/grpcx/consumer/ChannelFactory.java
+++ grpcx/grpcx_core/src/main/java/cn/ladd/grpcx/consumer/ChannelFactory.java
@@ -2,13 +2,16 @@ package cn.ladd.grpcx.consumer;
 
 import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import cn.ladd.grpcx.register.common.HostInfo;
 import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 
 public class ChannelFactory {
 	private static ConcurrentHashMap<HostInfo, ManagedChannel> serviceChannels=new ConcurrentHashMap<HostInfo, ManagedChannel>();
-	
+	private static Lock singlonLocker=new ReentrantLock();
 	
 	public static ManagedChannel getChannel(String serviceName)
 	{
@@ -19,7 +22,13 @@ public class ChannelFactory {
 		}
 		else 
 		{
-			return null;
+			singlonLocker.lock();
+			ManagedChannel channel=ManagedChannelBuilder.forAddress(hostInfo.getIp(), Integer.valueOf(hostInfo.getPort()))
+					.usePlaintext(true)
+					.build();
+			serviceChannels.put(hostInfo, channel);
+			singlonLocker.unlock();
+			return channel;
 		}
 		
 	}
