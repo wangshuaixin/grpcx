eb748cc4276af1baa53373e04ba33d5c82d92ce0
ladd <ladd.cn@gmail.com>
2017-12-25 09:30:46 +0800

    Add testcase for Config

---
+++ grpcx_core/src/test/java/cn/ladd/grpcx/config/ConfigTest.java
@@ -0,0 +1,22 @@
+package cn.ladd.grpcx.config;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ConfigTest {
+
+	@Test
+	public void test() {
+		assertTrue(Config.DEFAULT_HEARTBEAT_INTERVAL==Config.getHeartbeatInterval());
+		assertTrue(Config.DEFAULT_HEARTBEAT_NODECHECK_INTERVAL==Config.getHeartbeatNodecheckInterval());
+		assertTrue(Config.DEFAULT_HEARTBEAT_OVERDUE==Config.getHeartbeatOverdueSecond());
+		assertTrue(Config.DEFAULT_LOCAL_IP.equals(Config.getLocalIP()));
+		assertTrue(Config.DEFAULT_LOCAL_PORT==Config.getLocalPort());
+		assertTrue(Config.DEFAULT_REGISTER_IP.equals(Config.getRegisterIP()));
+		assertTrue(Config.DEFAULT_REGISTER_PORT==Config.getRegisterPort());
+		assertTrue((Config.DEFAULT_ZOOKEEPER_IP+":"+Config.DEFAULT_ZOOKEEPER_PORT).equals(Config.getZookeeperHost()));
+		
+	}
+
+}
